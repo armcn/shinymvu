195642073a2f14ed63fa705b877ffee3ef2642fe
Andrew R McNeil <andrew.richard.mcneil@gmail.com>
2026-04-01 12:34:21 -0400

    Update first-draft

--- first-draft/inst/www/shinymvu/bridge.js
+++ first-draft/inst/www/shinymvu/bridge.js
@@ -1,42 +1,42 @@
-(function() {
+(function () {
   "use strict";
 
   window.shinymvu = window.shinymvu || {};
 
-  shinymvu.register = function(config) {
-    document.addEventListener("alpine:init", function() {
-      Alpine.data(config.name, function() {
-        var component = {
-          model: {},
-          init: function() {
-            var self = this;
-            Shiny.addCustomMessageHandler(config.modelChannel, function(data) {
-              self.model = data;
-            });
-            if (config.handlers) {
-              var channels = Object.keys(config.handlers);
-              for (var i = 0; i < channels.length; i++) {
-                Shiny.addCustomMessageHandler(
-                  channels[i], config.handlers[channels[i]]
-                );
-              }
-            }
-          },
-          send: function(type, value) {
-            Shiny.setInputValue(
-              config.msgId,
-              { type: type, value: value === undefined ? null : value },
-              { priority: "event" }
-            );
-          },
-          shinySet: function(name) {
-            Shiny.setInputValue(name, Date.now(), { priority: "event" });
-          }
-        };
-        if (config.extend) {
-          Object.assign(component, config.extend);
-        }
-        return component;
+  function registerHandlers(handlers) {
+    Object.entries(handlers).forEach(function (entry) {
+      Shiny.addCustomMessageHandler(entry[0], entry[1]);
+    });
+  }
+
+  function createComponent(config) {
+    var component = {
+      model: {},
+      init: function () {
+        var self = this;
+        Shiny.addCustomMessageHandler(config.modelChannel, function (data) {
+          self.model = data;
+        });
+        if (config.handlers) registerHandlers(config.handlers);
+      },
+      send: function (type, value) {
+        Shiny.setInputValue(
+          config.msgId,
+          { type: type, value: value === undefined ? null : value },
+          { priority: "event" }
+        );
+      },
+      shinySet: function (name) {
+        Shiny.setInputValue(name, Date.now(), { priority: "event" });
+      }
+    };
+    return config.extend ? Object.assign(component, config.extend) : component;
+  }
+
+  shinymvu.register = function (config) {
+    document.addEventListener("alpine:init", function () {
+      Alpine.data(config.name, function () {
+        return createComponent(config);
       });
     });
   };
