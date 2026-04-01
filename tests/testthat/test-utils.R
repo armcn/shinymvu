e2be25ffb604b7e600c9716c5c7f233e96884de6
Andrew R McNeil <andrew.richard.mcneil@gmail.com>
2026-04-01 07:57:31 -0400

    Test tag utils directly

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,61 +1,41 @@
-test_inputs <- list(
-  tag       = shiny::tags$div(),
-  tag_list  = shiny::tagList(shiny::tags$div()),
-  tag_list2 = list(shiny::tags$div(), shiny::tags$span()),
-  html      = shiny::HTML("<b>bold</b>"),
-  icon      = shiny::icon("table"),
-  string    = "hello",
-  number    = 42,
-  null      = NULL,
-  bad_list  = list(1, 2, 3)
-)
-
-test_that("is_tag matches shiny:::isTag", {
-  for (nm in names(test_inputs)) {
-    expect_identical(
-      is_tag(test_inputs[[nm]]),
-      shiny:::isTag(test_inputs[[nm]]),
-      label = nm
-    )
-  }
+test_that("is_tag recognizes shiny tags", {
+  expect_true(is_tag(shiny::tags$div()))
+  expect_true(is_tag(shiny::tags$span("hello")))
+  expect_false(is_tag(shiny::tagList(shiny::tags$div())))
+  expect_false(is_tag(shiny::HTML("<b>bold</b>")))
+  expect_false(is_tag("hello"))
+  expect_false(is_tag(42))
+  expect_false(is_tag(NULL))
 })
 
-test_that("is_tag_list matches shiny:::isTagList (strict)", {
-  for (nm in names(test_inputs)) {
-    expect_identical(
-      is_tag_list(test_inputs[[nm]], strict = TRUE),
-      shiny:::isTagList(test_inputs[[nm]], strict = TRUE),
-      label = nm
-    )
-  }
+test_that("is_html recognizes HTML objects", {
+  expect_true(is_html(shiny::HTML("<b>bold</b>")))
+  expect_false(is_html(shiny::tags$div()))
+  expect_false(is_html("plain string"))
+  expect_false(is_html(42))
 })
 
-test_that("is_tag_list matches shiny:::isTagList (non-strict)", {
-  for (nm in names(test_inputs)) {
-    expect_identical(
-      is_tag_list(test_inputs[[nm]], strict = FALSE),
-      shiny:::isTagList(test_inputs[[nm]], strict = FALSE),
-      label = nm
-    )
-  }
+test_that("is_tag_list recognizes tag lists (strict)", {
+  expect_true(is_tag_list(shiny::tagList(shiny::tags$div())))
+  expect_false(is_tag_list(list(shiny::tags$div())))
+  expect_false(is_tag_list(shiny::tags$div()))
+  expect_false(is_tag_list("hello"))
 })
 
-test_that("is_tag_like matches shiny:::isTagLike (strict)", {
-  for (nm in names(test_inputs)) {
-    expect_identical(
-      is_tag_like(test_inputs[[nm]], strict = TRUE),
-      shiny:::isTagLike(test_inputs[[nm]], strict = TRUE),
-      label = nm
-    )
-  }
+test_that("is_tag_list recognizes tag lists (non-strict)", {
+  expect_true(is_tag_list(shiny::tagList(shiny::tags$div()), strict = FALSE))
+  expect_true(is_tag_list(list(shiny::tags$div(), shiny::tags$span()), strict = FALSE))
+  expect_false(is_tag_list(list(1, 2, 3), strict = FALSE))
+  expect_false(is_tag_list(shiny::tags$div(), strict = FALSE))
+  expect_false(is_tag_list("hello", strict = FALSE))
 })
 
-test_that("is_tag_like matches shiny:::isTagLike (non-strict)", {
-  for (nm in names(test_inputs)) {
-    expect_identical(
-      is_tag_like(test_inputs[[nm]], strict = FALSE),
-      shiny:::isTagLike(test_inputs[[nm]], strict = FALSE),
-      label = nm
-    )
-  }
+test_that("is_tag_like accepts tags, tag lists, and HTML", {
+  expect_true(is_tag_like(shiny::tags$div()))
+  expect_true(is_tag_like(shiny::HTML("<b>bold</b>")))
+  expect_true(is_tag_like(shiny::tagList(shiny::tags$div())))
+  expect_true(is_tag_like(shiny::icon("table")))
+  expect_false(is_tag_like("hello"))
+  expect_false(is_tag_like(42))
+  expect_false(is_tag_like(NULL))
 })
